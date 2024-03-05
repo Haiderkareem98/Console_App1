b1cf17c5d799e6a6a149effb625ac6ef3ef985e1
Haiderkareem98 <105423327+Haiderkareem98@users.noreply.github.com>
2024-03-05 13:23:52 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Hello, World!");
+// ------------------------------------------------
+ //   ------------------------------------------------
+
+ //string interpolation
+
+Console.WriteLine($"there are {10} user online now of {20}");
+
+Console.WriteLine("there are {0} user online now of ","haider");
+
+
+//datetime
+
+DateTime mydate = DateTime.Today;
+Console.WriteLine(mydate);
+
+//readLine
+
+Console.WriteLine("what is your name");
+string name = Console.ReadLine();
+Console.WriteLine("my name is "+name);
+
+Console.WriteLine("How old are you");
+int age = int.Parse(Console.ReadLine());
+
+//int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("my age is " + age);
+
